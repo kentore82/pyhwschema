716068e2739cb8c7a1e02a116f796dc833ac576c
Ken Tore Tallakstad <tallakstad@gmail.com>
2018-11-22 14:51:54 +0100

    Spark 2.4.0 compatible

--- pyhwschema/avrotospark.py
+++ pyhwschema/avrotospark.py
@@ -2,9 +2,10 @@ import json
 from pyspark.sql.types import StructType
 
 
-def avro_to_spark_schema(pyspark_session, avro_schema_string):
+def avro_to_spark_schema(pyspark_session, avro_schema_string, spark_version='2.4.0'):
     """Takes an avro-schema string as input and returns pyspark struct-type
 
+      For Spark version < 2.4.0:
       The spark package com.databricks.spark.avro needs to be on sparks classpath.
       E.g.:
           --packages com.databricks:spark-avro_2.11:4.0.0
@@ -21,15 +22,20 @@ def avro_to_spark_schema(pyspark_session, avro_schema_string):
 
           pyspark_session (pyspark.sql.SparkSession): A spark session
           avro_schema_string (str): Input Avro schema
+          spark_version (str): default '2.4.0'
 
       Returns:
           pyspark_struct_type (pyspark.sql.types.StructType): A pyspark StructType schema
     """
 
-    java_avro_schema=pyspark_session._jvm.org.apache.avro.Schema.parse(avro_schema_string)
+    java_avro_schema = pyspark_session._jvm.org.apache.avro.Schema.parse(avro_schema_string)
 
-    pyspark_struct_type_json=pyspark_session._jvm.com.databricks.spark.avro.SchemaConverters \
-          .toSqlType(java_avro_schema).dataType().json()
+    if (spark_version != '2.4.0') or (spark_version != u'2.4.0'):
+        pyspark_struct_type_json = pyspark_session._jvm.com.databricks.spark.avro.SchemaConverters\
+            .toSqlType(java_avro_schema).dataType().json()
+    else:
+        pyspark_struct_type_json = pyspark_session._jvm.org.apache.spark.sql.avro.SchemaConverters\
+            .toSqlType(java_avro_schema).dataType().json()
 
     pyspark_struct_type = StructType.fromJson(json.loads(pyspark_struct_type_json))
 
